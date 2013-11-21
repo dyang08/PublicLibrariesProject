b19cc6c72b69478f87a032503e957e3ff7b83c95
Oscar <ormg1909@gmail.com>
2013-11-21 10:47:49 -0700

    Update DataWriter.java
    simple utility lets you write to DB01 database
    constructor( your DB01 username and Password)

--- Library/src/library/DataWriter.java
+++ Library/src/library/DataWriter.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * simple utility lets you write to DB01 database
+ * Author oscar menendez
  */
 
 package library;
